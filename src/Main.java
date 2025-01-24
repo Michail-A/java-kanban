380beb9990df99e763f2e0ae5bfba858a3459cc5
Michail <m.ananev2014@mail.ru>
2025-01-24 23:17:19 +0300

    Рефакторинг методов

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
-import manager.FileBackedTaskManager;
-import manager.Managers;
-import model.Task;
-import util.Constants;
-
 public class Main {
 
     public static void main(String[] args) {
-        FileBackedTaskManager manager = new FileBackedTaskManager(Managers.getDefaultHistory());
-        Task task = new Task("1", "2");
-        manager.addTask(task);
-        manager.getTaskById(task.getId());
-
     }
 }
